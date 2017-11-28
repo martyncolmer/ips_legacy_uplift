b53e617e80e842ba21b98f9d96c7c54c00ef5bea
Mahoney <mahont1@ons.gov.uk>
2017-11-28 14:43:34 +0000

    Data manipulation functionality added.

--- Mahoney/LoadSAS.py
+++ Mahoney/LoadSAS.py
@@ -3,11 +3,6 @@ import pandas as pd
 import pandas.util.testing as tm; tm.N = 3
 import numpy as np   
 
-
-
-
-
-
 ##Used to Transpose the Dataset into a vertical format.
 def unpivot(frame):
     N, K = frame.shape
@@ -40,12 +35,19 @@ def create_survey_column(frame):
 def create_survey_value(frame):
     N, K = frame.shape
     data = {
+            'VERSION_ID' : np.asarray(versionid).repeat(N),
             'COLUMN_VALUE'   : frame.values.ravel('F'),
             'COLUMN_NO'  : np.asarray(numbers).repeat(N),
-            'SERIAL_NO'  : np.tile(np.asarray(frame['Serial']), K)
+            'SERIAL_NO'  : np.tile(np.asarray(frame['Serial']), K)#.astype(int)
             }
-    return pd.DataFrame(data, columns=['SERIAL_NO','COLUMN_NO','COLUMN_VALUE'])  
-      
+    return pd.DataFrame(data, columns=['VERSION_ID','SERIAL_NO','COLUMN_NO','COLUMN_VALUE'])  
+
+def remove_decimal(text):
+    varType = type(text)
+    if(varType == float):
+        if(text%1 == 0):
+            return int(text)
+    return text
         
 sasFile = 'testdata_2.sas7bdat'
 
@@ -57,34 +59,63 @@ for i, col in enumerate(file.columns, 1):
     tmp = [i, col.name, col.type, col.length,col.format, col.label]
     columnProperties.append(tmp)
     
-types = []
-lengths = []
-formats = []
-labels = []
-numbers = []
+types,lengths,formats,labels,numbers = []
+
 for x in columnProperties:
     numbers.append(x[0])
     types.append(x[2])
     lengths.append(x[3])
     formats.append(x[4])
     labels.append(x[5])
+    
 numbers.pop(0)
+versionid = ['9999']* len(numbers)
 types.pop(0)
 lengths.pop(0)
 formats.pop(0)
 labels.pop(0)   
 
 df = file.to_data_frame()
-N,K = df.shape
-
 
 pdf = create_survey_value(df).sort_values(by= ['SERIAL_NO','COLUMN_NO'])
 
+pdf2 = pdf
+pdf2['SERIAL_NO'] = pdf2['SERIAL_NO'].apply(remove_decimal)
+pdf2['COLUMN_VALUE'] = pdf2['COLUMN_VALUE'].apply(remove_decimal)
+csvOut = pdf2.to_csv(index = False,header = False)
 
-csvOut = pdf.to_csv(index = False)
+
+
+
+
+
+
+
+
+
+import El.IPSTransformation.CommonFunctions as elStuff
+
+func =  elStuff.IPSCommonFunctions()
+cur = func.get_oracle_connection() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 print(csvOut)
 
-file = open("Output2.txt",'w')
-file.write(csvOut)
-file.close()
+
