07d37498bb5ced26a9bf9c368d8c8184ede2efc6
Richmond Rice <richmond.rice@ons.gsi.gov.uk>
2017-11-29 17:25:22 +0000

    R

---
+++ Rich/R.R
@@ -0,0 +1,110 @@
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/tidyverse_1.1.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/haven_1.1.0.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/Rcpp_0.12.11.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/forcats_0.2.0.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/magrittr_1.5.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/tibble_1.3.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/rlang_0.1.4.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/readr_1.1.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/R6_2.2.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/hms_0.3.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/data.table_1.10.4-3.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/bin/windows/contrib/3.4/reshape2_1.4.2.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/RJDBC_0.2-5.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/DBI_0.7.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/rJava_0.9-9.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/broom_0.4.3.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/dplyr_0.7.4.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/assertthat_0.2.0.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/bindrcpp_0.2.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/bindr_0.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/glue_1.2.0.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/pkgconfig_2.0.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/tidyr_0.7.2.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/purrr_0.2.4.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/plyr_1.8.4.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/psych_1.7.8.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/mnormt_1.5-5.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/stringr_1.2.0.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/stringi_1.1.6.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/ggplot2_2.2.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/gtable_0.2.0.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/scales_0.5.0.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/munsell_0.4.3.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/colorspace_1.3-2.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/lazyeval_0.2.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/httr_1.3.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/jsonlite_1.5.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/lubridate_1.7.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/modelr_0.1.1.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/readxl_1.0.0.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/cellranger_1.1.0.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/rvest_0.3.2.zip")
+#install.packages("http://ricer:APAvBKmgWnwPvNuuCdQmUeXMHZZ@art-p-01/artifactory/vr-R/3.4/xml2_1.1.1.zip")
+
+library(RJDBC)
+library(haven)
+library(tidyverse)
+library(data.table)
+
+saszipinput <- "\\\\nsdata3\\Social_Surveys_team\\CASPA\\IPS\\Testing\\testdata.zip" # path of sas zip test data
+#saszipinput <- "\\\\nsdata3\\Social_Surveys_team\\CASPA\\IPS\\Testing\\quarter12017.zip" # path of sas zip real data
+systime <- format(Sys.time(), "%Y%m%d%H%M%S") # timestamp for unique folder for unzip location
+saszipoutput <- paste("D:\\#", systime, sep = "") # path of unzip location
+csvinput <- "\\\\nsdata3\\Social_Surveys_team\\CASPA\\IPS\\Testing\\Non Response Q1 2017.csv" # path of csv
+
+unzip(saszipinput, exdir = saszipoutput) # unzip sas data from share to local
+
+#Sys.sleep(10) # temp pause for testing
+
+unzippedsas <- list.files(path = saszipoutput, pattern = "*.sas", full.names = FALSE, recursive = FALSE) # list sas files unzipped
+
+if (length(unzippedsas) == 0) # error if no sas files found
+{
+  print("error - no sas")
+}
+
+if (length(unzippedsas) > 1) # error if more than one sas file found
+{
+  print("error - too much sas")
+}
+
+if (length(unzippedsas) == 1) # import the sas file if only one file found
+{
+  print("importing sas")
+  sasconcat <- paste(saszipoutput, "\\", unzippedsas, sep = "") # concatenate full file path of sas to be imported
+  sasdata <- read_sas(sasconcat) # import the sas file
+  #print(sasdatadf)
+}
+
+unlink(saszipoutput, recursive = TRUE) # delete local unzipped data
+
+#csvimport <- read.csv(csvinput) # read csv
+#print(csvimport)
+
+
+
+
+
+# transpose data
+
+#print(melt(sasdata, id=c("Serial")))
+
+
+
+
+
+# connect to oracle using jdbc - roracle is probably better but it needs to be compiled
+#jdbcDriver <- JDBC(driverClass="oracle.jdbc.OracleDriver", classPath="d:/r/lib/ojdbc6.jar")
+#jdbcConnection <- dbConnect(jdbcDriver, "jdbc:oracle:thin:@//exa01-scan.ons.statistics.gov.uk:1521/DEVCON", "CASPA_1_POWELD2_DATA", "CASPA_1_POWELD2")
+#testQuery <- dbGetQuery(jdbcConnection, "SELECT * from CASPA_1_POWELD2_DATA.SAS_PARAMETERS") # test query
+#dbDisconnect(jdbcConnection)
+#print(testQuery) # print test query
+
+# write data to oracle test
+#jdbcDriver <- JDBC(driverClass="oracle.jdbc.OracleDriver", classPath="d:/r/lib/ojdbc6.jar")
+#jdbcConnection <- dbConnect(jdbcDriver, "jdbc:oracle:thin:@//exa01-scan.ons.statistics.gov.uk:1521/DEVCON", "IPS_1_POWELD2_DATA", "IPS_1_POWELD2")
+#surcoltestdf <- data.frame(VERSION_ID = 1, COLUMN_NO = c(1,2,3), COLUMN_DESC = c("Shiftno","Questno","Serial"), COLUMN_TYPE = c("number", "number", "number"), COLUMN_LENGTH = c(4,4,6))
+#dbWriteTable(jdbcConnection, "SURVEY_COLUMN", surcoltestdf, append=TRUE, overwrite=FALSE)
+
+dbDisconnect(jdbcConnection)
