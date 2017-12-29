da9e0426acbf6beb3ae7cea07d3ccf2c931a5439
Amin <aminf@ons.gov.uk>
2017-12-29 14:17:15 +0000

    Changes made to the import and export functions based on recommendations by James

--- Faisal/sas_data_export.py
+++ Faisal/sas_data_export.py
@@ -8,37 +8,59 @@ import cx_Oracle
 import filecmp
  
 
-def sas_data_export(id_number):
+def sas_data_export(file_id):
     
     """
     Author     : Faisal
     Date       : 18 Dec 2017
     Purpose    : Writes SAS or .CSV file to Oracle table
-    Parameters : Expects id number 
-    Returns    : None        
+    Parameters : id number to be assigned to the file being loaded into database.  
+    Returns    : Returns nothing        
     """
     
     path_to_export_data = r"\\nsdata3\Social_Surveys_team\CASPA\IPS\Testing\export_data\export_data_out.sas7bdat"
     
-    filename, ext = os.path.splitext(path_to_export_data)
-    # check file format
-    if (ext.lower() != ".sas7bdat" and ext.lower() != ".csv"):
-        raise Exception("Abort: File format not supported")
-     
-    # open and read file as bytes
-    content = ''
-    with open (path_to_export_data,'rb') as file:
-        content = file.read()
-
-    conn = CommonFunctions.get_oracle_connection(credentials_file = r"\\nsdata3\Social_Surveys_team\CASPA\IPS\IPSCredens.json")
-    db = conn.cursor()
-    db.setinputsizes (blobData = cx_Oracle.BLOB)
+    filename, extension = os.path.splitext(path_to_export_data)
     
-    sqlStr = "insert into  sas_data_export VALUES(:id,'subsample', :blobData)"
-    db.execute (sqlStr, id = id_number,blobData= content)
-    db.execute ('commit') 
+    # check file format i.e if not .SAS or .CSV then raise exception
+    if (extension.lower() != ".sas7bdat" and extension.lower() != ".csv"):
+        raise Exception("Abort: File format not supported")
+    
+     
+    # open and read file in binary mode
+    content = ''
+    
+    if os.path.exists(path_to_export_data):
+        with open (path_to_export_data,'rb') as file:
+            content = file.read()
+    else:
+        raise Exception('Error: File not found')
+                
+    conn = CommonFunctions.get_oracle_connection(credentials_file = r"\\nsdata3\Social_Surveys_team\CASPA\IPS\IPSCredens.json")
+    db_connection = conn.cursor()
+    
+    
+    sqlString = "SELECT  SAS_PROCESS_ID FROM SAS_DATA_EXPORT"
+    result  = db_connection.execute (sqlString)
+    column_values = result.fetchall()
+    column_values = [x[0] for x in column_values]
+
+    if column_values:
+        
+        if file_id in column_values:
+            print('This id number already used. Please provide different file id')
+    
+        else:
+            
+            file_to_copy = filename.split('\\')[-1] + extension
+            
+            sqlStr = "insert into  sas_data_export VALUES(:id, :filecopy, :blobData)"
+            db_connection.setinputsizes (blobData = cx_Oracle.BLOB)
+            db_connection.execute (sqlStr, id = file_id,blobData= content, filecopy = file_to_copy)
+            db_connection.execute ('commit')
+     
     # # close the Oracle database connection
-    db.close()
+    db_connection.close()
     
     
 def sas_data_import(file_id):
@@ -47,42 +69,41 @@ def sas_data_import(file_id):
     Author     : Faisal
     Date       : 19 Dec 2017
     Purpose    : Reads .SAS or .CSV file from Oracle table and writes it to a file on local disk
-    Parameters : Expects id number 
-    Returns    : None        
+    Parameters : id number of the file to be retrieved from database. In this case, value of the column SAS_PROCESS_ID from SAS_DATA_EXPORT table 
+    Returns    : Returns nothing        
     """
     
     conn = CommonFunctions.get_oracle_connection(credentials_file = r"\\nsdata3\Social_Surveys_team\CASPA\IPS\IPSCredens.json")
-    db = conn.cursor()
-    sqlStr = "SELECT SDE_LABEL, SDE_DATA FROM SAS_DATA_EXPORT WHERE SAS_PROCESS_ID =" + str(file_id)
-    result  = db.execute (sqlStr)
+    db_connection = conn.cursor()
+    sqlStr = "SELECT  SDE_LABEL, SDE_DATA FROM SAS_DATA_EXPORT WHERE SAS_PROCESS_ID =" + str(file_id)
+    result  = db_connection.execute (sqlStr)
     content = result.fetchall()
-    file_name = content[0][0]
-    
-    ext = file_name.split('.')[-1]
+     
+    if content:
         
-    with open (file_name, 'wb') as file:
-            file.write(str(content[0][1]))
+        file_name = content[0][0]
+        try:    
+            with open (file_name, 'wb') as file:
+                    file.write(str(content[0][1]))
             
-    db.close()
+        except IOError as io_err:
+            
+                print(io_err)
+       
+        #for comparison to check if file retrieved was of same as file exported to database. True if files are same and False otherwise
+        file1 =  r"\\nsdata3\Social_Surveys_team\CASPA\IPS\Testing\export_data\export_data_out.sas7bdat"
+        print(filecmp.cmp(file1, file_name, shallow = False) )
     
-    
-    #for comparison to check if file retrieved was of correct
-    file1 =  r"\\nsdata3\Social_Surveys_team\CASPA\IPS\Testing\export_data\export_data_out.sas7bdat"
+    else:
+        
+        print('No contents found for file id [ ' + str(file_id) + ' ]')   
+             
+    db_connection.close()
+ 
+#comment out the line below for testing i.e if exporting then comment sas_data_import and vice versa...
 
-    
-#     with open (file1,'rb') as file:
-#         content1 = file.read()
-#         
-#     with open ('importedFile.sas7bdat', 'rb') as file:
-#         content2 = file.read()
-    
-    #Compares two files and returns true if same file
-        
-    print(filecmp.cmp(file1, file_name, shallow = False) )
-        
-#test run
-#sas_data_export(1111)    
-sas_data_import(999)    
+sas_data_export(1111)    
+#sas_data_import(6666666666666666)    
     
         
     
