adf3377a773db3c4aec22d52370d4d46f2bb8bc7
James Burr <burrj@statistics.gov.uk>
2018-01-29 14:24:57 +0000

    Completed the do_ips_nrweight_calculation function and output datasets are confirmed correct. Stored procedure still has some code commented out currently.

--- IPS_Stored_Procedures/calculate_ips_nonresponse_weight.py
+++ IPS_Stored_Procedures/calculate_ips_nonresponse_weight.py
@@ -17,25 +17,27 @@ def do_ips_nrweight_calculation():
     Date         : Jan 2018
     Purpose      : Performs calculations to find the nonresponse weight.
     Parameters   : NA
-    Returns      : 
+    Returns      : NA
     Requirements : 
     Dependencies : 
     """
     
-    # Add the shift weight
+    
     df_nonresponsedata_sorted = df_nonresponsedata.sort_values(colset1)
     df_surveydata_sorted = df_surveydata.sort_values(colset1)
     
     df_psw = df_surveydata_sorted.groupby(colset1)\
             ['SHIFT_WT'].agg({'SHIFT_WT' : 'mean'})
     
+    # Flattens the column structure
     df_psw = df_psw.reset_index()
     
     # Only keep rows that exist in df_nonresponsedata_sorted 
     df_grossmignonresp = pd.merge(df_nonresponsedata_sorted, df_psw, on=\
             colset1, how='left')
     
-    # Add gross values using the primary sampling weight
+    # Add gross values using the primary sampling weight and add two new columns
+    # to df_grossmignonresp
     df_grossmignonresp['grossmignonresp'] = df_grossmignonresp['SHIFT_WT'] * \
             df_grossmignonresp['MIGTOTAL']
             
@@ -54,10 +56,13 @@ def do_ips_nrweight_calculation():
     df_summignonresp = df_summignonresp.rename(columns = {'grossordnonresp' : 'grossinelresp'})
     
     # Calculate the grossed number of respondents over the non-response strata
+    
+    # Use only records in which NR_FLAG_PV is 0
     df_surveydata_sliced = df_surveydata.loc[df_surveydata['NR_FLAG_PV'] == 0]
     
     df_surveydata_sorted = df_surveydata_sliced.sort_values(by=['NR_PORT_GRP_PV', 'ARRIVEDEPART'])
     
+    # Create two new columns as aggregations of SHIFT_WT
     df_sumresp = df_surveydata_sorted.groupby(['NR_PORT_GRP_PV', 'ARRIVEDEPART'])\
             ['SHIFT_WT'].agg({\
             'gross_resp' : 'sum',
@@ -68,12 +73,12 @@ def do_ips_nrweight_calculation():
     
     # Calculate the grossed number of T&T non-respondents of the non-response strata    
     
-    # Use onely records from the survey dataset where the NR_FLAG_PV is 1    
+    # Use only records from the survey dataset where the NR_FLAG_PV is 1, then sort    
     df_surveydata_sliced = df_surveydata.loc[df_surveydata['NR_FLAG_PV'] == 1]
     
     df_surveydata_sorted = df_surveydata_sliced.sort_values(by=['NR_PORT_GRP_PV', 'ARRIVEDEPART'])     
     
-    # Create new column using the sum of ShiftWt (evaluates from var_PSW)
+    # Create new column using the sum of ShiftWt
     df_sumordnonresp = df_surveydata_sorted.groupby(['NR_PORT_GRP_PV', 'ARRIVEDEPART'])\
             ['SHIFT_WT'].agg({\
             'grossordnonresp' : 'sum'})
@@ -81,7 +86,7 @@ def do_ips_nrweight_calculation():
     # Flattens the column structure after adding the new grossordnonresp column
     df_sumordnonresp = df_sumordnonresp.reset_index()
     
-    # Sort values in the three dataframes
+    # Sort values in the three dataframes required for the next calculation
     df_sumordnonresp = df_sumordnonresp.sort_values(by=['NR_PORT_GRP_PV', 'ARRIVEDEPART'])
     
     df_sumresp = df_sumresp.sort_values(by=['NR_PORT_GRP_PV', 'ARRIVEDEPART'])
@@ -89,7 +94,6 @@ def do_ips_nrweight_calculation():
     df_summignonresp = df_summignonresp.sort_values(by=['NR_PORT_GRP_PV', 'ARRIVEDEPART'])
      
     # Use the calculated data frames to calculate the non-response weight
-    #df_gnr_with_drops = df_gnr.drop('j', 'x', 'z', 'errorStr') may be required to drop these values from unprepared table
     
     # Merge previously sorted dataframes into one, ensuring all rows from summignonresp are kept
     df_gnr = df_summignonresp.merge(df_sumresp, on=['NR_PORT_GRP_PV', 'ARRIVEDEPART'], how = 'outer')
@@ -114,49 +118,61 @@ def do_ips_nrweight_calculation():
     # Ensure only complete or partial responses are kept
     df_surveydata_sorted = df_surveydata_sorted.loc[df_surveydata_sorted['NR_FLAG_PV'] == 0]
     
-    # Produce summary by merging survey data and gnr data together
+    # Produce summary by merging survey data and gnr data together, then sort
     df_out = df_surveydata_sorted.merge(df_gnr[['NR_PORT_GRP_PV',
                                                 'ARRIVEDEPART', 
                                                 'non_response_wt']],
                                         on=['NR_PORT_GRP_PV',
                                             'ARRIVEDEPART'],
                                         how = 'left')
-    
-    # Produce summary output
+
     df_out = df_out.sort_values(by=['NR_PORT_GRP_PV', 'ARRIVEDEPART'])
     
-    # Setup for aggregation function; outlines variables to work on, columns to\
-    # add and functions to perform
-
+    # Create and add three new columns calculated using SHIFT_WT
     df_summary = df_out.groupby(['NR_PORT_GRP_PV',
                                 'ARRIVEDEPART',
                                 'WEEKDAY_END_PV'])['SHIFT_WT'].agg({'mean_resps_sh_wt' : 'mean',\
                                                                    'count_resps' : 'count',\
                                                                    'prior_sum' : 'sum'})
     
+    # Flatten column structure
     df_summary.reset_index(inplace = True)
     
+    
+    # Create and add one new column calculated using 'non_response_wt' in a 
+    # different dataframe due to difficulty in creating all four new columns
+    # simultaneously in a single dataframe
     df_summary_nr = df_out.groupby(['NR_PORT_GRP_PV',
                                 'ARRIVEDEPART',
                                 'WEEKDAY_END_PV'])['non_response_wt'].agg({'mean_nr_wt' : 'mean'})
     
+    # Flatten column structure
     df_summary_nr.reset_index(inplace = True)
     
+    # Merge all four new columns into the same dataframe
     df_summary = df_summary.merge(df_summary_nr, on = ['NR_PORT_GRP_PV',
                                           'ARRIVEDEPART',
                                           'WEEKDAY_END_PV'],
                      how = 'outer')
     
+    # Merge the updated dataframe with specific columns from GNR.
     df_summary = df_gnr[['NR_PORT_GRP_PV',
                                         'ARRIVEDEPART',
                                         'gnr',
-                                        'gross_resp']].merge(df_summary,\
+                                        'gross_resp']].merge(df_summary,
                                   on = ['NR_PORT_GRP_PV',
                                         'ARRIVEDEPART'], how='outer')
+                                        
+    # Calculate new non_response_wt value if condition is met
+    df_out['non_response_wt'] = np.where(df_out['MIG_FLAG_PV'] == 0,
+                                 (df_out['non_response_wt'] * df_out['TANDTSI'])
+                                / df_out['MIGSI'], df_out['non_response_wt'])
     
-    df_summary.to_csv('finalsummary.csv')
+    # Reduce output to just key value pairs
+    df_out = df_out[['SERIAL', 'non_response_wt']]
     
-
+    
+    
 # Call JSON configuration file for error logger setup
 survey_support.setup_logging('IPS_logging_config_debug.json')
 
