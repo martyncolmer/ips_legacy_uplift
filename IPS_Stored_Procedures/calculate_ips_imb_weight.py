57b533d44bede76eab04c907362246a380abf638
Thorne <thorne1@ons.gov.uk>
2018-02-13 16:01:43 +0000

    End of working day 13th Feb

--- IPS_Stored_Procedures/calculate_ips_imb_weight.py
+++ IPS_Stored_Procedures/calculate_ips_imb_weight.py
@@ -37,7 +37,7 @@ def do_ips_imbweight_calculation():
     # Change column names to upper case (just in case)
     df_survey_data.columns = df_survey_data.columns.str.upper()
     
-    # If col IMBAL_ELIGIBLE_PV == 1.0 then col IMBAL_WT = 1.0
+    # Do some initial setup and selection  
     df_survey_data.loc[df_survey_data["IMBAL_ELIGIBLE_PV"] == 1, "IMBAL_WT"] = 1.0
     
     # FUDGE SOME VALUES - TO BE REMOVED WHEN TEST DATASET IS COMPLETE
@@ -74,31 +74,30 @@ def do_ips_imbweight_calculation():
 #    sys.exit()
 
     # Update output with provisional imbalance weight for overseas departures
-    df_overseas_deps = df_survey_data.copy()
-    flow_condition = (df_overseas_deps["FLOW"] == 1) | (df_overseas_deps["FLOW"] == 5)
-    arrivedepart_condition = df_overseas_deps["ARRIVEDEPART"] == 2
-    df_overseas_deps.loc[(flow_condition) & (arrivedepart_condition)
-                         , "IMBAL_WT"] = df_overseas_deps["IMBAL_PORT_FACT_PV"]                               
-#    print df_overseas_deps[["FLOW","ARRIVEDEPART","IMBAL_WT", "IMBAL_PORT_FACT_PV"]].head()
-#    print df_overseas_deps.info()
+    flow_condition = (df_survey_data["FLOW"] == 1) | (df_survey_data["FLOW"] == 5)
+    arrivedepart_condition = df_survey_data["ARRIVEDEPART"] == 2
+    df_survey_data.loc[(flow_condition) & (arrivedepart_condition)
+                         , "IMBAL_WT"] = df_survey_data["IMBAL_PORT_FACT_PV"]                               
+#    print df_survey_data[["FLOW","ARRIVEDEPART","IMBAL_WT", "IMBAL_PORT_FACT_PV"]].head()
+#    print df_survey_data.info()
 #    sys.exit()
     
     # Update output with provisional imbalance weight for overseas arrivals
-    flow_condition = (df_overseas_deps["FLOW"] == 3) | (df_overseas_deps["FLOW"] == 7)
-    arrivedepart_condition = df_overseas_deps["ARRIVEDEPART"] == 1
-    df_overseas_deps.loc[(flow_condition) & (arrivedepart_condition)
-                         , "IMBAL_WT"] = df_overseas_deps["IMBAL_PORT_FACT_PV"]
-#    print df_overseas_deps[["FLOW","ARRIVEDEPART","IMBAL_WT","IMBAL_PORT_FACT_PV"]].head()
-#    print df_overseas_deps.info()
+    flow_condition = (df_survey_data["FLOW"] == 3) | (df_survey_data["FLOW"] == 7)
+    arrivedepart_condition = df_survey_data["ARRIVEDEPART"] == 1
+    df_survey_data.loc[(flow_condition) & (arrivedepart_condition)
+                         , "IMBAL_WT"] = df_survey_data["IMBAL_PORT_FACT_PV"]
+#    print df_survey_data[["FLOW","ARRIVEDEPART","IMBAL_WT","IMBAL_PORT_FACT_PV"]].head()
+#    print df_survey_data.info()
 #    sys.exit()
     
     # Update overseas departures with country imbalance
-    flow_condition = (df_overseas_deps["FLOW"] == 1) | (df_overseas_deps["FLOW"] == 5)
-    df_overseas_deps.loc[(flow_condition)
-                         , "IMBAL_WT"] = (df_overseas_deps["IMBAL_WT"]
-                                           * df_overseas_deps["IMBAL_CTRY_FACT_PV"])
-#    print df_overseas_deps[["FLOW","IMBAL_WT","IMBAL_CTRY_FACT_PV"]].head()
-#    print df_overseas_deps.info()
+    flow_condition = (df_survey_data["FLOW"] == 1) | (df_survey_data["FLOW"] == 5)
+    df_survey_data.loc[(flow_condition)
+                         , "IMBAL_WT"] = (df_survey_data["IMBAL_WT"]
+                                           * df_survey_data["IMBAL_CTRY_FACT_PV"])
+#    print df_survey_data[["FLOW","IMBAL_WT","IMBAL_CTRY_FACT_PV"]].head()
+#    print df_survey_data.info()
 #    sys.exit()
 
     # Calculate the pre and post sums for overseas residents
@@ -172,20 +171,51 @@ def do_ips_imbweight_calculation():
     
     # Select data, where conditions
     df_temp = df_overseas_residents.loc[all_conditions]
-    df_calc_imb_weighting = df_temp.copy()
+    df_calc_departures = df_temp.copy()
 
     # Set "DIFFERENCE" and "RATIO" column and values
-    df_calc_imb_weighting["DIFFERENCE"] = pd.Series(df_temp["POST_IMB_WEIGHTS"] 
+    df_calc_departures["DIFFERENCE"] = pd.Series(df_temp["POST_IMB_WEIGHTS"] 
                                                    - df_temp["PRE_IMB_WEIGHTS"])
-    df_calc_imb_weighting["RATIO"] = pd.Series(df_calc_imb_weighting["DIFFERENCE"] 
+    df_calc_departures["RATIO"] = pd.Series(df_calc_departures["DIFFERENCE"] 
                                                    / df_total_traffic["TOT_NI_TRAFFIC"])
     
     # Delete unnecessary columns 
-    del df_calc_imb_weighting["POST_IMB_WEIGHTS"]
-    del df_calc_imb_weighting["PRE_IMB_WEIGHTS"]
+    del df_calc_departures["POST_IMB_WEIGHTS"]
+    del df_calc_departures["PRE_IMB_WEIGHTS"]
 #
-#    print df_calc_imb_weighting
+#    print df_calc_departures
 #    sys.exit()
+
+    # Calculate the imbalance weight
+
+    
+    # Set conditions of first and second inner nested select
+    flow_range1 = [2,4,6,8]
+    flow_range2 = [1,3,5,7]
+    condition1 = df_survey_data["PORTROUTE"].isin(df_calc_departures["PORTROUTE"])
+    condition2 = ((df_survey_data["FLOW"].isin(flow_range1)) | (df_calc_departures["FLOW"].isin(flow_range2)))
+    nested_condition = ((condition1) & (condition2))
+    
+    
+        
+    
+#    UPDATE df_survey_data
+#    SET df_survey_data['IMBAL_WT'] = 1.0 - (SELECT df_calc_departures["RATIO"] 
+#                                            FROM df_calc_departures
+        #                                        WHERE df_survey_data["PORTROUTE"] = df_calc_departures["PORTROUTE"]
+        #                                              & ((df_survey_data["FLOW"] = 2 & df_calc_departures["FLOW"] = 1) |
+        #                                                   (df_survey_data["FLOW"] = 4 & df_calc_departures["FLOW"] = 3) |
+        #                                                    (df_survey_data["FLOW"] = 6 & df_calc_departures["FLOW"] = 5) |
+        #                                                   (df_survey_data["FLOW"] = 8 & df_calc_departures["FLOW"] = 7)))
+#    WHERE EXISTS (SELECT * FROM df_calc_departures
+#                    WHERE df_survey_data["PORTROUTE"] = df_calc_departures["PORTROUTE"]
+#                          & ((df_survey_data["FLOW"] = 2 & df_calc_departures["FLOW"] = 1) |
+#                               (df_survey_data["FLOW"] = 4 & df_calc_departures["FLOW"] = 3) |
+#                                (df_survey_data["FLOW"] = 6 & df_calc_departures["FLOW"] = 5) |
+#                               (df_survey_data["FLOW"] = 8 & df_calc_departures["FLOW"] = 7)));
+    
+
+    
     
 
 def calc_imb_weight(oracle_dataset = True):
