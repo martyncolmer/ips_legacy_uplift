4ff386cb3219c49671f93b19625ec70afd0c0c52
Adnan <adnan.fiaz@ons.gsi.gov.uk>
2018-03-13 11:59:34 +0000

    removed numpy import from spend imputation test file and moved test files from network location to repository

--- TestSuite/Calculations/test_calculate_ips_spend_imputation.py
+++ TestSuite/Calculations/test_calculate_ips_spend_imputation.py
@@ -4,13 +4,12 @@ Created on 12 Mar 2018
 @author: thorne1
 '''
 import pandas as pd
-import numpy as np
 from pandas.util.testing import assert_frame_equal
 from IPS_Stored_Procedures import calculate_ips_spend_imputation as spend
 
 def test_calculate():
     # This is an integration test as it runs the entire step
-    test_survey = pd.read_pickle(r'\\nsdata3\Social_Surveys_team\CASPA\IPS\Testing\Spend Imputation\Pickles\surveydata.pkl')
+    test_survey = pd.read_pickle('TestSuite/data/spend_imp_surveydata.pkl')
 
     # Retrieve Python output data
     py_survey_data = spend.do_ips_spend_imputation(test_survey
@@ -38,19 +37,14 @@ def test_calculate():
     # Cleanse
     py_survey_data = py_survey_data.sort_values(by = 'SERIAL')
     py_survey_data.index = range(0, len(py_survey_data))
-    
+
     # Retrieve SAS Survey Data output and cleanse
-    test_result_survey = pd.read_pickle(r'\\nsdata3\Social_Surveys_team\CASPA\IPS\Testing\Spend Imputation\Pickles\output_merge_eligible.pkl')
+    test_result_survey = pd.read_pickle('TestSuite/data/spend_imp_output_merge_eligible.pkl')
     test_result_survey.columns = test_result_survey.columns.str.upper()
     test_result_survey = test_result_survey.sort_values(by = 'SERIAL')
     test_result_survey.index = range(0, len(test_result_survey))
-#    test_result_survey.replace("", np.NaN, inplace=True)
-    
+
     # Assert dfs are equal
     assert_frame_equal(py_survey_data
                        , test_result_survey
                        , check_column_type=False)
-    
-if __name__ == "__main__":
-    test_calculate()
-    
