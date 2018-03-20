50b3c862c2c66857a6107f338493b2ad3131ed89
Adnan <adnan.fiaz@ons.gsi.gov.uk>
2018-03-20 13:53:02 +0000

    moved test data to tests/data and added test data for rail imputation. updated rail imputation test to not use calculate function. moved import_traffic_data to tests/io

--- tests/calculations/test_calculate_ips_rail_impute.py
+++ tests/calculations/test_calculate_ips_rail_impute.py
@@ -6,27 +6,26 @@ Created on 05 March 2018
 
 import pandas as pd
 from pandas.util.testing import assert_frame_equal
-from IPS_Test_Modules.calculate_ips_rail_impute import calculate
+from main.calculations.calculate_ips_rail_impute import do_ips_railex_imp
 
 def test_calculate():
     # This is an integration test as it runs the entire step
 
-    test_survey = pd.read_pickle(r'\\nsdata3\Social_Surveys_team\CASPA\IPS\Testing\Rail Imputation\surveydata.pkl')
+    test_survey = pd.read_pickle('data/rail_imp_input.pkl')
 
-    result_data = calculate(test_survey,
-                            OutputData = 'SAS_RAIL_IMP', 
-                            ResponseTable = 'SAS_RESPONSE', 
+    result_data = do_ips_railex_imp(test_survey,
+                            output = 'SAS_RAIL_IMP',
                             var_serial = 'SERIAL', 
-                            var_flow = 'FLOW', 
+                            var_eligible = 'FLOW',
                             var_fweight = 'FINAL_WT', 
                             var_count = 'COUNT',
                             strata = ['FLOW', 
                                       'RAIL_CNTRY_GRP_PV'],
-                            var_railexercise = 'RAIL_EXERCISE_PV',
+                            var_railfare = 'RAIL_EXERCISE_PV',
                             var_spend = 'SPEND',
                             minCountThresh = 30)
 
-    test_result_summary = pd.read_pickle(r'\\nsdata3\Social_Surveys_team\CASPA\IPS\Testing\Rail Imputation\output_test.pkl')
+    test_result_summary = pd.read_pickle('data/rail_imp_output.pkl')
     test_result_summary.columns = test_result_summary.columns.str.upper()
     
     test_result_summary = test_result_summary.sort_values(by = 'SERIAL')
