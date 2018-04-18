f00fb6f6c392f596fe97a32d921546802c31449f
James Burr <burrj@statistics.gov.uk>
2018-04-18 13:53:25 +0100

    Fix code execution errors
    Errors that prevented the code from running (such as name not found errors) have been removed.
    
    
    Former-commit-id: e3d62ee8ecd17f3dcf48707c493adcc3faac81e6

--- main/utils/PickleRick.py
+++ main/utils/PickleRick.py
@@ -6,8 +6,8 @@ Created on 5 Mar 2018
 import pandas as pd
 
 
-in_path = r'\\nsdata3\Social_Surveys_team\CASPA\IPS\Testing\Regional Weights\output_final.sas7bdat'
-out_path = r'../tests/data/regional_wt_output.pkl'
+in_path = r'\\nsdata3\Social_Surveys_team\CASPA\IPS\Testing\Calculate_Non_Response_Weight\surveydata_1.sas7bdat'
+out_path = r'../../tests/data/non_response_survey_data.pkl'
 
 df = pd.read_sas(in_path)
 
---
+++ tests/calculations/test_calculate_ips_nonresponse_weight.py
@@ -0,0 +1,67 @@
+'''
+Created on 17th April 2018
+
+@author: James Burr
+'''
+
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+from main.calculations import calculate_ips_nonresponse_weight as non_resp
+
+
+def test_calculate():
+    # This is an integration test as it runs the entire step
+    test_survey = pd.read_pickle('../data/non_response_survey_data.pkl')
+    test_nr_data = pd.read_pickle('../data/non_response_data.pkl')
+    result_py_data = non_resp.do_ips_nrweight_calculation(test_survey,
+                                                          test_nr_data,
+              OutputData='SAS_NON_RESPONSE_WT',
+              SummaryData='SAS_PS_NON_RESPONSE',
+              ResponseTable='SAS_RESPONSE',
+              NRStratumDef=['NR_PORT_GRP_PV',
+                            'ARRIVEDEPART'],
+              ShiftsStratumDef=['NR_PORT_GRP_PV',
+                                'ARRIVEDEPART',
+                                'WEEKDAY_END_PV'],
+              var_NRtotals='MIGTOTAL',
+              var_NonMigTotals='ORDTOTAL',
+              var_SI='',
+              var_migSI='MIGSI',
+              var_TandTSI='TANDTSI',
+              var_PSW='SHIFT_WT',
+              var_NRFlag='NR_FLAG_PV',
+              var_migFlag='MIG_FLAG_PV',
+              var_respCount='COUNT_RESPS',
+              var_NRWeight='NON_RESPONSE_WT',
+              var_meanSW='MEAN_RESPS_SH_WT',
+              var_priorSum='PRIOR_SUM',
+              var_meanNRW='MEAN_NR_WT',
+              var_grossResp='GROSS_RESP',
+              var_gnr='GNR',
+              var_serialNum='SERIAL',
+              minCountThresh='30')
+
+    # Retrieve and clean Python survey data output
+    py_survey_data = result_py_data[0]
+    py_survey_data = py_survey_data.sort_values(by = 'SERIAL')
+    py_survey_data.index = range(0, len(py_survey_data))
+
+    # From Python output data, retrieve Summary Data and cleanse
+    py_summary_data = result_py_data[1]
+    py_summary_data.index = range(0, len(py_summary_data))
+
+    # Retrieve SAS Survey Data output and cleanse
+    test_result_survey = pd.read_pickle('../data/non_response_weight_output.pkl')
+    test_result_survey.columns = test_result_survey.columns.str.upper()
+    test_result_survey = test_result_survey.sort_values(by='SERIAL')
+    test_result_survey.index = range(0, len(test_result_survey))
+    #test_result_survey.replace("", np.NaN, inplace=True)
+
+    # Retrieve SAS Summary Data output and cleanse
+    test_result_summary = pd.read_pickle('../data/non_response_weight_summary.pkl')
+    test_result_summary.columns = test_result_summary.columns.str.upper()
+    test_result_summary.index = range(0, len(test_result_summary))
+
+    # Assert dfs are equal
+    assert_frame_equal(py_survey_data, test_result_survey, check_dtype=False)
+    assert_frame_equal(py_summary_data, test_result_summary, check_dtype=False)
