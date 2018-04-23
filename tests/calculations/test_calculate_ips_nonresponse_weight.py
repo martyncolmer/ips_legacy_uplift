32c61b491a00bcd6bd1edc63a183f87b23b9f1c0
Adnan Fiaz <adnan.fiaz@ons.gsi.gov.uk>
2018-04-23 08:42:03 +0100

    In non response weight optimized imports and replaced function arguments with constants at the top of the module with some minor renaming. Also updated documentation and test.
    Former-commit-id: 056283880d325f216b0c8799a79d4135e2bba099

--- tests/calculations/test_calculate_ips_nonresponse_weight.py
+++ tests/calculations/test_calculate_ips_nonresponse_weight.py
@@ -6,46 +6,20 @@ Created on 17th April 2018
 
 import pandas as pd
 from pandas.util.testing import assert_frame_equal
-from main.calculations import calculate_ips_nonresponse_weight as non_resp
 
-from main.io import CommonFunctions as cf
+from main.calculations import calculate_ips_nonresponse_weight as non_resp
 
 
 def test_calculate():
     # This is an integration test as it runs the entire step
     test_survey = pd.read_pickle('../data/non_response_survey_data.pkl')
     test_nr_data = pd.read_pickle('../data/non_response_data.pkl')
-    result_py_data = non_resp.do_ips_nrweight_calculation(test_survey,
-                                                          test_nr_data,
-              OutputData='SAS_NON_RESPONSE_WT',
-              SummaryData='SAS_PS_NON_RESPONSE',
-              ResponseTable='SAS_RESPONSE',
-              NRStratumDef=['NR_PORT_GRP_PV',
-                            'ARRIVEDEPART'],
-              ShiftsStratumDef=['NR_PORT_GRP_PV',
-                                'ARRIVEDEPART',
-                                'WEEKDAY_END_PV'],
-              var_NRtotals='MIGTOTAL',
-              var_NonMigTotals='ORDTOTAL',
-              var_SI='',
-              var_migSI='MIGSI',
-              var_TandTSI='TANDTSI',
-              var_PSW='SHIFT_WT',
-              var_NRFlag='NR_FLAG_PV',
-              var_migFlag='MIG_FLAG_PV',
-              var_respCount='COUNT_RESPS',
-              var_NRWeight='NON_RESPONSE_WT',
-              var_meanSW='MEAN_RESPS_SH_WT',
-              var_priorSum='PRIOR_SUM',
-              var_meanNRW='MEAN_NR_WT',
-              var_grossResp='GROSS_RESP',
-              var_gnr='GNR',
-              var_serialNum='SERIAL',
-              minCountThresh='30')
+    result_py_data = non_resp.do_ips_nrweight_calculation(test_survey, test_nr_data,
+                                                          'NON_RESPONSE_WT', 'SERIAL')
 
     # Retrieve and clean Python survey data output
     py_survey_data = result_py_data[0]
-    py_survey_data = py_survey_data.sort_values(by = 'SERIAL')
+    py_survey_data = py_survey_data.sort_values(by='SERIAL')
     py_survey_data.index = range(0, len(py_survey_data))
 
     # From Python output data, retrieve Summary Data and cleanse
@@ -57,7 +31,7 @@ def test_calculate():
     test_result_survey.columns = test_result_survey.columns.str.upper()
     test_result_survey = test_result_survey.sort_values(by='SERIAL')
     test_result_survey.index = range(0, len(test_result_survey))
-    #test_result_survey.replace("", np.NaN, inplace=True)
+    # test_result_survey.replace("", np.NaN, inplace=True)
 
     # Retrieve SAS Summary Data output and cleanse
     test_result_summary = pd.read_pickle('../data/non_response_weight_summary.pkl')
