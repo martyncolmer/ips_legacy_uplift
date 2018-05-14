aafd847576e8c42478f9421a3f4553b55cf37b84
Adnan Fiaz <adnan.fiaz@ons.gsi.gov.uk>
2018-05-14 09:11:44 +0100

    for imbalance weight commented out import of survey support and cleaned up imports
    Former-commit-id: b87d768f23ee94e0545de0a91d7de09e9d2daad7

--- main/calculations/calculate_ips_imb_weight.py
+++ main/calculations/calculate_ips_imb_weight.py
@@ -4,12 +4,8 @@ Created on 7 Feb 2018
 @author: thorne1
 '''
 import inspect
-# import survey_support
-
 import pandas as pd
-import survey_support
 
-# import survey_support
 from main.io import CommonFunctions as cf
 
 OUTPUT_TABLE_NAME = "SAS_IMBALANCE_WT"
