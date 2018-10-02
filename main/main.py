f9a985394419c9025065b88f7cea91817f850135
Thorne <thorne1@ons.gov.uk>
2018-10-02 15:08:14 +0100

    Stay Imputation Step refactored but untested.
    Former-commit-id: 09fa4585e8cb9372cdb6f7a4f65f6e9886b05556

--- main/main.py
+++ main/main.py
@@ -461,22 +461,41 @@ def stay_imputation_step(run_id,connection):
     Dependencies : NA
     """
 
-    step = "STAY_IMPUTATION"
+    # Load configuration variables
+    step_name = "STAY_IMPUTATION"
 
-    generic_xml_steps.populate_survey_data_for_step(run_id, connection, step)
-    generic_xml_steps.copy_step_pvs_for_survey_data(run_id, connection, step)
+    # Populate Survey Data For Stay Imputation
+    idm.populate_survey_data_for_step(run_id, connection, STEP_CONFIGURATION[step_name])
 
+    # Copy Stay Imp PVs For Survey Data
+    idm.copy_step_pvs_for_survey_data(run_id, connection, STEP_CONFIGURATION[step_name])
+
+    # Apply Stay Imp PVs On Survey Data
     process_variables.process(dataset='survey',
                               in_table_name='SAS_SURVEY_SUBSAMPLE',
                               out_table_name='SAS_STAY_SPV',
                               in_id='serial')
 
-    generic_xml_steps.update_survey_data_with_step_pv_output(connection, step)
+    # Update Survey Data with Stay Imp PV Output
+    idm.update_survey_data_with_step_pv_output(connection, STEP_CONFIGURATION[step_name])
 
-    calculate_ips_stay_imputation.ips_stay_imp()
+    # Retrieve data from SQL
+    survey_data = cf.get_table_values(idm.SAS_SURVEY_SUBSAMPLE_TABLE)
 
-    generic_xml_steps.update_survey_data_with_step_results(connection, step)
-    generic_xml_steps.store_survey_data_with_step_results(run_id, connection, step)
+    # Calculate Stay Imputation
+    survey_data_out = calculate_ips_stay_imputation.do_ips_stay_imputation(survey_data,
+                                                                           var_serial='SERIAL',
+                                                                           num_levels=1,
+                                                                           measure='mean')
+
+    # Insert data to SQL
+    cf.insert_dataframe_into_table(STEP_CONFIGURATION[step_name]["temp_table"], survey_data_out)
+
+    # Update Survey Data With Stay Imp Results
+    idm.update_survey_data_with_step_results(connection, STEP_CONFIGURATION[step_name])
+
+    # Store Survey Data With Stay Imp Results
+    idm.store_survey_data_with_step_results(run_id, connection, STEP_CONFIGURATION[step_name])
 
 
 def fares_imputation_step(run_id, connection):
