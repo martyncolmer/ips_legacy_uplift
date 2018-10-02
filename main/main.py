fc5f490fcda77214afc9a6b62ec45d5cc078b838
Thorne <thorne1@ons.gov.uk>
2018-10-02 14:28:36 +0100

    Traffic Weight Step refactored but untested AND includes TODO.  Once Traffic Weight step has been refactored to include R-GES, correct paramaters need to be passed to do_ips_trafficweight_calculation().
    Former-commit-id: ac70277599d800082b39dc3e7de12737be1b57d6

--- main/main.py
+++ main/main.py
@@ -229,32 +229,58 @@ def traffic_weight_step(run_id, connection):
     Dependencies : NA
     """
 
-    step = "TRAFFIC_WEIGHT"
+    # Load configuration variables
+    step_name = "TRAFFIC_WEIGHT"
 
-    generic_xml_steps.populate_survey_data_for_step(run_id, connection, step)
-    generic_xml_steps.populate_step_data(run_id, connection, step)
-    generic_xml_steps.copy_step_pvs_for_survey_data(run_id, connection, step)
+    # Populate Survey Data For Traffic Wt
+    idm.populate_survey_data_for_step(run_id, connection, STEP_CONFIGURATION[step_name])
 
+    # Populate Traffic Data
+    idm.populate_step_data(run_id, connection, STEP_CONFIGURATION[step_name])
+
+    # Copy Traffic Wt PVs For Survey Data
+    idm.copy_step_pvs_for_survey_data(run_id, connection, STEP_CONFIGURATION[step_name])
+
+    # Apply Traffic Wt PV On Survey Data
     process_variables.process(dataset='survey',
                               in_table_name='SAS_SURVEY_SUBSAMPLE',
                               out_table_name='SAS_TRAFFIC_SPV',
                               in_id='serial')
 
-    generic_xml_steps.update_survey_data_with_step_pv_output(connection, step)
-    generic_xml_steps.copy_step_pvs_for_step_data(run_id, connection, step)
+    # Update Survey Data with Traffic Wt PV Output
+    idm.update_survey_data_with_step_pv_output(connection, STEP_CONFIGURATION[step_name])
 
+    # Copy Traffic Wt PVs For Traffic Data
+    idm.copy_step_pvs_for_step_data(run_id, connection, STEP_CONFIGURATION[step_name])
+
+    # Apply Traffic Wt PV On Traffic Data
     process_variables.process(dataset='traffic',
-                              in_table_name='SAS_NON_RESPONSE_DATA',
+                              in_table_name='SAS_TRAFFIC_DATA',
                               out_table_name='SAS_TRAFFIC_PV',
                               in_id='REC_ID')
 
-    generic_xml_steps.update_survey_data_with_step_pv_output(connection, step)
+    # Update Traffic Data With Traffic Wt PV Output
+    idm.update_survey_data_with_step_pv_output(connection, STEP_CONFIGURATION[step_name])
 
-    calculate_ips_traffic_weight.calculate()
+    # Retrieve data from SQL
+    survey_data = cf.get_table_values(idm.SAS_SURVEY_SUBSAMPLE_TABLE)
 
-    generic_xml_steps.update_survey_data_with_step_results(connection, step)
-    generic_xml_steps.store_survey_data_with_step_results(run_id, connection, step)
-    generic_xml_steps.store_step_summary(run_id, connection, step)
+    # TODO: Pass correct parameters to do_ips_trafweight_calculation() once step has been refactored to include new R-GES
+    # Calculate Traffic Weight
+    output_data, summary_data = calculate_ips_traffic_weight.do_ips_trafweight_calculation(survey_data)
+
+    # Insert data to SQL
+    cf.insert_dataframe_into_table(STEP_CONFIGURATION[step_name]["temp_table"], output_data)
+    cf.insert_dataframe_into_table(STEP_CONFIGURATION[step_name]["sas_ps_table"], summary_data)
+
+    # Update Survey Data With Traffic Wt Results
+    idm.update_survey_data_with_step_results(connection, STEP_CONFIGURATION[step_name])
+
+    # Store Survey Data With Traffic Wt Results
+    idm.store_survey_data_with_step_results(run_id, connection, STEP_CONFIGURATION[step_name])
+
+    # Store Traffic Wt Summary
+    idm.store_step_summary(run_id, connection, STEP_CONFIGURATION[step_name])
 
 
 def unsampled_weight_step(run_id, connection):
