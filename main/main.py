a39fbccfa2524e6a79d3d0e6b1a927870d67ff37
Thorne <thorne1@ons.gov.uk>
2018-10-02 14:47:45 +0100

    Imbalance Weight Step refactored but untested.
    Former-commit-id: 337180d1d508df0569c4ac1a1b0f78e537956bb4

--- main/main.py
+++ main/main.py
@@ -359,23 +359,48 @@ def imbalance_weight_step(run_id, connection):
     Dependencies : NA
     """
 
-    step = "IMBALANCE_WEIGHT"
+    # Load configuration variables
+    step_name = "IMBALANCE_WEIGHT"
 
-    generic_xml_steps.populate_survey_data_for_step(run_id, connection, step)
-    generic_xml_steps.copy_step_pvs_for_survey_data(run_id, connection, step)
+    # Populate Survey Data For Imbalance Wt
+    idm.populate_survey_data_for_step(run_id, connection, STEP_CONFIGURATION[step_name])
 
+    # Copy Imbalance Wt PVs For Survey Data
+    idm.copy_step_pvs_for_survey_data(run_id, connection, STEP_CONFIGURATION[step_name])
+
+    # Apply Imbalance Wt PVs On Survey Data
     process_variables.process(dataset='survey',
                               in_table_name='SAS_SURVEY_SUBSAMPLE',
                               out_table_name='SAS_IMBALANCE_SPV',
                               in_id='serial')
 
-    generic_xml_steps.update_survey_data_with_step_pv_output(connection, step)
+    # Update Survey Data With Imbalance Wt PVs Output
+    idm.update_survey_data_with_step_pv_output(connection, STEP_CONFIGURATION[step_name])
 
-    calculate_ips_imb_weight.calculate()
+    # Retrieve data from SQL
+    survey_data = cf.get_table_values(idm.SAS_SURVEY_SUBSAMPLE_TABLE)
 
-    generic_xml_steps.update_survey_data_with_step_results(connection, step)
-    generic_xml_steps.store_survey_data_with_step_results(run_id, connection, step)
-    generic_xml_steps.store_step_summary(run_id, connection, step)
+    # Calculate Imbalance Weight
+    survey_data_out, summary_data_out = calculate_ips_imb_weight.do_ips_imbweight_calculation(survey_data,
+                                                                                              var_serialNum="SERIAL",
+                                                                                              var_shiftWeight="SHIFT_WT",
+                                                                                              var_NRWeight="NON_RESPONSE_WT",
+                                                                                              var_minWeight="MINS_WT",
+                                                                                              var_trafficWeight="TRAFFIC_WT",
+                                                                                              var_OOHWeight="UNSAMP_TRAFFIC_WT",
+                                                                                              var_imbalanceWeight="IMBAL_WT")
+    # Insert data to SQL
+    cf.insert_dataframe_into_table(STEP_CONFIGURATION[step_name]["temp_table"], survey_data_out)
+    cf.insert_dataframe_into_table(STEP_CONFIGURATION[step_name]["sas_ps_table"], summary_data_out)
+
+    # Update Survey Data With Imbalance Wt Results
+    idm.update_survey_data_with_step_results(connection, STEP_CONFIGURATION[step_name])
+
+    # Store Survey Data With Imbalance Wt Results
+    idm.store_survey_data_with_step_results(run_id, connection, STEP_CONFIGURATION[step_name])
+
+    # Store Imbalance Weight Summary
+    idm.store_step_summary(run_id, connection, STEP_CONFIGURATION[step_name])
 
 
 def final_weight_step(run_id, connection):
