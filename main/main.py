7f94f423aa915a68a3fc3d2686fcc006ece69006
Thorne <thorne1@ons.gov.uk>
2018-10-02 15:23:34 +0100

    Spend Imputation Step refactored but untested.
    Former-commit-id: 5473ec439a2d8947b79d65d836d27d3a00ad5d26

--- main/main.py
+++ main/main.py
@@ -559,22 +559,40 @@ def spend_imputation_step(run_id, connection):
     Dependencies : NA
     """
 
-    step = "SPEND_IMPUTATION"
+    # Load configuration variables
+    step_name = "SPEND_IMPUTATION"
 
-    generic_xml_steps.populate_survey_data_for_step(run_id, connection, step)
-    generic_xml_steps.copy_step_pvs_for_survey_data(run_id, connection, step)
+    # Populate Survey Data For Spend Imputation
+    idm.populate_survey_data_for_step(run_id, connection, STEP_CONFIGURATION[step_name])
 
+    # Copy Spend Imp PVs For Survey Data
+    idm.copy_step_pvs_for_survey_data(run_id, connection, STEP_CONFIGURATION[step_name])
+
+    # Apply Spend Imp PVs On Survey Data
     process_variables.process(dataset='survey',
                               in_table_name='SAS_SURVEY_SUBSAMPLE',
                               out_table_name='SAS_SPEND_SPV',
                               in_id='serial')
 
-    generic_xml_steps.update_survey_data_with_step_pv_output(connection, step)
+    # Update Survey Data with Spend Imp PV Output
+    idm.update_survey_data_with_step_pv_output(connection, STEP_CONFIGURATION[step_name])
 
-    calculate_ips_spend_imputation.calculate()
+    # Retrieve data from SQL
+    survey_data = cf.get_table_values(idm.SAS_SURVEY_SUBSAMPLE_TABLE)
 
-    generic_xml_steps.update_survey_data_with_step_results(connection, step)
-    generic_xml_steps.store_survey_data_with_step_results(run_id, connection, step)
+    # Calculate Spend Imputation
+    survey_data_out = calculate_ips_spend_imputation.do_ips_spend_imputation(survey_data,
+                                                                             var_serial="SERIAL",
+                                                                             measure="mean")
+
+    # Insert data to SQL
+    cf.insert_dataframe_into_table(STEP_CONFIGURATION[step_name]["temp_table"], survey_data_out)
+
+    # Update Survey Data With Spend Imp Results
+    idm.update_survey_data_with_step_results(connection, STEP_CONFIGURATION[step_name])
+
+    # Store Survey Data With Spend Imp Results
+    idm.store_survey_data_with_step_results(run_id, connection, STEP_CONFIGURATION[step_name])
 
 
 def rail_imputation_step(run_id, connection):
