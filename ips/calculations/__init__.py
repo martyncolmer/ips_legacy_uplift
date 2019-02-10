907fe8df8f637c6bd679443bf06a67a48086f6c1
Paul Soule <paul.soule@ext.ons.gov.uk>
2019-02-10 08:42:03 +0000

    Revert previous - solution is to run tests under WSL

--- ips/calculations/__init__.py
+++ ips/calculations/__init__.py
@@ -5,10 +5,9 @@ import pandas as pd
 
 
 def parallelise_dataframe(df, func):
-    num_cores = multiprocessing.cpu_count() - 1  # leave one free to not freeze machine
-    num_partitions = num_cores  # number of partitions to split dataframe
+    num_partitions = multiprocessing.cpu_count()
     df_split = np.array_split(df, num_partitions)
-    pool = multiprocessing.Pool(num_cores)
+    pool = multiprocessing.Pool(num_partitions)
 
     df = pd.concat(pool.map(func, df_split))
 
